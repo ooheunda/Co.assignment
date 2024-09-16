4d95ec303f9fce4cbf7d64a87857eb1dfa821e1e
ooheunda <ooheunda@gmail.com>
2024-09-16 20:13:15 +0900

    :wrench: chore: 서버 포트 환경 변수로 관리

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
 const bootstrap = async (): Promise<void> => {
   const app = await NestFactory.create(AppModule);
-
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-  await app.listen(3000);
+  const PORT = await app.get(ConfigService).get('SERVER_PORT');
+  await app.listen(PORT);
 };
 bootstrap();
