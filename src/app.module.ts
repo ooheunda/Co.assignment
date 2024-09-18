894c37c519d75bd93b736b2fc946212e91333b85
ooheunda <ooheunda@gmail.com>
2024-09-18 19:50:10 +0900

    :wrench: chore: TypeORM 연결 #3

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,38 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import Joi from 'joi';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as Joi from 'joi';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+const typeOrmModuleOptions = {
+  useFactory: async (configService: ConfigService): Promise<TypeOrmModuleOptions> => ({
+    namingStrategy: new SnakeNamingStrategy(),
+    type: 'mysql',
+    host: configService.get('DB_HOST'),
+    port: configService.get('DB_PORT'),
+    username: configService.get('DB_USERNAME'),
+    password: configService.get('DB_PASSWORD'),
+    database: configService.get('DB_NAME'),
+    synchronize: configService.get('DB_SYNC'),
+    entities: [],
+    logging: true,
+  }),
+  inject: [ConfigService],
+};
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync(typeOrmModuleOptions),
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema: Joi.object({
         SERVER_PORT: Joi.number().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+        DB_SYNC: Joi.boolean().required(),
       }),
     }),
   ],
