76896c85aea9cab34b3063e8ec75a35330414043
ooheunda <ooheunda@gmail.com>
2024-09-20 13:36:53 +0900

    :wrench: chore: api prefix 설정

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import { AppModule } from './app.module';
 
 const bootstrap = async (): Promise<void> => {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.setGlobalPrefix('/api');
 
   const PORT = await app.get(ConfigService).get('SERVER_PORT');
   await app.listen(PORT);
