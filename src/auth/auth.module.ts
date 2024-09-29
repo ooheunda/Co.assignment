bbed371893f5360563beb9e358506a0679d34bd1
ooheunda <ooheunda@gmail.com>
2024-09-29 18:49:26 +0900

    :recycle: refactor: Jwt 모듈을 한 곳에서 관리하도록 Common 모듈 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,24 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { CommonModule } from '../common/common.module';
 import { User } from '../common/entities/user.entity';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([User]),
-    JwtModule.registerAsync({
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get('JWT_SECRET'),
-        signOptions: { expiresIn: '1d' },
-      }),
-      inject: [ConfigService],
-    }),
-  ],
+  imports: [TypeOrmModule.forFeature([User]), CommonModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
---
+++ src/common/common.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  imports: [
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '1d' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [],
+  exports: [JwtModule],
+})
+export class CommonModule {}
